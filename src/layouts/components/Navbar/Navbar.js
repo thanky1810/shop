85a06e022cae01e50e8a5fdc9cd61c74c451b2e6
Than Van Ky <thanky4444.com>
2025-01-06 23:26:29 +0700

    navBarUi

--- src/layouts/components/Navbar/Navbar.js
+++ src/layouts/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from './Navbar.module.scss'
+import { faFacebook, faInstagram, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+
 
 const cx = classNames.bind(styles)
 
@@ -12,7 +15,12 @@ function Navbar() {
                 <a className={cx('topbar-item')}>Kênh người bán</a>
                 <a className={cx('topbar-item')}>Trở Thành Người Bán Shopee</a>
                 <a className={cx('topbar-item')}>Tải Ứng Dụng</a>
-                <div className={cx('topbar-item')}>Kết nối</div>
+                <div className={cx('topbar-item')}> 
+                    <div >Kết nối</div>
+                    <FontAwesomeIcon className={cx('icon')} icon={faFacebook} />
+                    <FontAwesomeIcon className={cx('icon')} icon={faInstagram} />
+                </div>
+
             </div>
         </div>
     );

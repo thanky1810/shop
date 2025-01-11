5af435cc77051dd4b17362db30626db488764240
Than Van Ky <thanky4444.com>
2025-01-11 19:05:09 +0700

    Searchbar UI#3.1

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 
 import styles from './Search.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import images from '~/assets/images'
 
 
@@ -13,8 +13,11 @@ function Search() {
         <div className={cx('wrapper')}>
             <div className={cx('search-container')}>
                 <img className={cx('shopee-logo')} src={images.logo} alt='shopee' />
-                <input className={cx('search-bar')} placeholder='Shopee bao ship 0Đ -  Đăng ký ngay!'>
-                </input>
+                <input className={cx('search-bar')} placeholder='Shopee bao ship 0Đ -  Đăng ký ngay!' />
+                <button className={cx('button-search')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+
                 <div className={cx('search-cart')}>
                     <FontAwesomeIcon className={cx('search-cart-icon')} icon={faCartShopping} />
                 </div>
